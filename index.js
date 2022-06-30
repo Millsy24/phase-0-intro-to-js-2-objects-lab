cd91d759d3e35384ed0d755e1888377f03bf882a
Lukas Mills <lukascodes64@gmail.com>
2022-06-30 17:32:58 -0400

    commit tree ce0946d0733e6c8853b4634a4916379de0b65748

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+let employee = {
+    name: 'Lukas',
+    streetAddress: 'Washingtion St',
+}
+
+function updateEmployeeWithKeyAndValue  (employee){
+  return employee.name = employee
+  
+}
+updateEmployeeWithKeyAndValue ('Sam')
+
+console.log(employee.name)
